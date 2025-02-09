38db48927e1f4060b0b4c726b0e4beec6cda1aeb
Shizukise <j.chavarria121121@hotmail.com>
2025-02-09 17:25:23 +0100

    backend deploy test

--- frontend/src/components/PlanPage.js
+++ frontend/src/components/PlanPage.js
@@ -3,28 +3,43 @@ import L from "leaflet"; // Import Leaflet
 import "leaflet/dist/leaflet.css"; // Import Leaflet CSS
 
 function PagePlan() {
-  const mapRef = useRef(null); // Create a ref to store the map instance
-  const mapContainer = useRef(null); // Create a ref for the div
-
-  useEffect(() => {
-    if (mapRef.current) return; // Prevent duplicate map creation
-
-    mapRef.current = L.map(mapContainer.current).setView([51.505, -0.09], 13);
     
-    L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
-      attribution: "© OpenStreetMap contributors"
-    }).addTo(mapRef.current);
-  }, []);
+    const API_BASE_URL = "https://trackme-backend.onrender.com";
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        {/* Use ref instead of id */}
-        <div ref={mapContainer} style={{ height: "500px", width: "100%" }}></div>
-        <button id="testbtn">Click</button>
-      </header>
-    </div>
-  );
+    const mapRef = useRef(null); // Create a ref to store the map instance
+    const mapContainer = useRef(null); // Create a ref for the div
+
+    useEffect(() => {
+        if (mapRef.current) return; // Prevent duplicate map creation
+
+        mapRef.current = L.map(mapContainer.current).setView([51.505, -0.09], 13);
+
+        L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+            attribution: "© OpenStreetMap contributors"
+        }).addTo(mapRef.current);
+    }, []);
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                {/* Use ref instead of id */}
+                <div ref={mapContainer} style={{ height: "500px", width: "100%" }}></div>
+                <button id="testbtn" onClick={async () => {
+                    try {
+                        const result = await fetch(`${API_BASE_URL}/api/get-location` , {
+                            method:'GET'
+                        });
+                        if (result.status === "200") {
+                            console.log(result.user)
+                        }
+                    } catch (err) {
+                        console.error(err)
+                    }
+                    }
+                }>Get current location</button>
+            </header>
+        </div>
+    );
 }
 
 export default PagePlan;
